808c75c1e4ca4bfdaefb2b8e7cb2d8b99e46444d
Beatriz Gonçalves <beatriz.gsouza@eaportal.org>
2024-03-17 09:41:25 -0300

    instalação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'orange': '#e28609',
+      'greem':'#006971',
+      'dark-green': '#004140',
+      'offwhite':'rgba(255, 255, 255, 0.75)',
+      'white':'#fff',
+      'offwhitebg':'#f2f2f2',
+    }
+  },
+  plugins: [],
+}
+
